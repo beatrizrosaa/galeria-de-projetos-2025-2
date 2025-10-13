d370225ad4071694d5ad259d39d7677095aa99a2
Ariel Menezes <69123486+ArielMAJ@users.noreply.github.com>
2025-10-13 08:23:12 -0300

    fix: adjust portfolio URL

--- portfolios/index.js
+++ portfolios/index.js
@@ -24,7 +24,7 @@ const portfolios = [
     title: "Portfólio em React (TS)",
     description: "",
     github: "https://github.com/tuca-janahu/tuca-janahu",
-    live: "https://portfolio-react-blush-beta.vercel.app/", // URL do site em produção. Se não funcionar, deixe vazio e adicione uma screenshot
+    live: "https://tucajanahu.app/", // URL do site em produção. Se não funcionar, deixe vazio e adicione uma screenshot
     screenshot: "", // URL de imagem opcional; se vazio, será mostrado o iframe do site
     authorName: "Artur Janahú",
     authorPhoto: "https://avatars.githubusercontent.com/u/143747367?v=4", // URL de foto opcional. Utilize o do seu perfil do GitHub.
